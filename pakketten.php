a48695c8ce944dce7bf90f3d3a9a56d0fe6ac0c2
ayoub <laachachayoub2@outlook.com>
2024-03-27 16:32:39 +0100

    begin aan pakketten

--- pakketten.php
+++ pakketten.php
@@ -1,10 +1,59 @@
 <?php
+
 include('db.php');
 
 if(!isset($_SESSION['gebruikersnaam'])) {
     header("location:login.php");
 }
 
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "maaskantje";
+
+// Verbinding maken met de database
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Controleren op fouten bij de verbinding
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (isset($_POST['submit'])) {
+        // Controleren of alle vereiste velden zijn ingevuld
+        if (isset($_POST['pakket_idpakket'], $_POST['product_idproduct'], $_POST['aantal'])) {
+            $pakket_idpakket = $_POST['pakket_idpakket'];
+            $product_idproduct = $_POST['product_idproduct'];
+            $aantal = $_POST['aantal'];
+            $selectedproducts = isset($_POST['product']) ? $_POST['product'] : [];
+
+            // Voorbereiden van de query om gegevens in te voegen
+            $stmt = $conn->prepare("INSERT INTO pakket_has_product (pakket_idpakket, product_idproduct, aantal, naam) VALUES (?, ?, ?, ?)");
+            $stmt->bind_param("iii", $pakket_idpakket, $product_idproduct, $aantal);
+            $stmt->execute();
+
+            // Loop over geselecteerde producten en voeg ze toe aan de relatie
+            foreach ($selectedproducts as $selectedproduct) {
+                $stmt = $conn->prepare("INSERT INTO pakket_has_product (product_idproduct, pakket_idpakket) VALUES (?, ?)");
+                $stmt->bind_param("ii", $selectedproduct, $pakket_idpakket);
+                $stmt->execute();
+            }
+        }
+    }
+
+    // Voeg nieuwe pakketten toe
+    if (isset($_POST['addpakket'])) {
+        if (isset($_POST['newpakket'])) {
+            $newpakket = $_POST['newpakket'];
+            $stmt = $conn->prepare("INSERT INTO pakket_has_product (pakket_idpakket) VALUES (?)");
+            $stmt->bind_param("i", $newpakket);
+            $stmt->execute();
+        } else {
+            echo "Nieuw pakket veld is niet ingevuld.";
+        }
+    }
+}
 ?>
 
 <!DOCTYPE html>
@@ -57,13 +106,34 @@ if(!isset($_SESSION['gebruikersnaam'])) {
         </div>
     </div>
 
-    <!-- ----------------------------------------------------------------------------------------------------------- -->
+    <div class="aanmaakpagina">
+        <form class="formsborder" method="post" action="" enctype="multipart/form-data">
+            Naam: <br><input type="text" name="naam" required><br>
+            Aantal: <br><input type="number" name="aantal" required><br><br>
+            Producten:<br>
+            <?php
+            // Haal producten op uit de database
+            $result = $conn->query("SELECT * FROM product");
+            if ($result->num_rows > 0) {
+                while ($row = $result->fetch_assoc()) {
+                    echo "<input type='checkbox' name='product[]' value='" . $row['idproduct'] . "'>" . $row['product'] . "<br>";
+                }
+            } else {
+                echo "Geen producten gevonden.";
+            }
+            ?>
+            <br><input class="formsbutton" type="submit" name="submit" value="Toevoegen">
+            <?php if (isset($_POST['submit'])) {echo "Product toegevoegd!"; } ?>
+        </form>
 
-    <p>Pakketten</p>
-
-    <!-- ----------------------------------------------------------------------------------------------------------- -->
+        <form class="formsborder" method="post" action="">
+            Nieuwe pakket: <input type="text" name="newpakket" required>
+            <input class="formsbutton" type="submit" name="addpakket" value="Toevoegen">
+        </form>
+    </div>
 
     <footer>
     </footer>
 </body>
+
 </html>
